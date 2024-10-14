2a0223af0e0421cf634cfcbe8f2fa85ee2e4567c
Samim Arefin <samimarefin@gmail.com>
2024-10-14 11:34:39 +0600

    added cart api

---
+++ Microservice/Services/Cart.API/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using Cart.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.API.DtoModels;
+
+namespace Cart.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly IRedisCacheService _redisCacheService;
+        public CartController(IRedisCacheService redisCacheService) => 
+            _redisCacheService = redisCacheService;
+
+        [HttpGet, Route("key")]
+        [ResponseCache(Duration = 10)]
+        public async Task<IActionResult> GetCarts(string key, CancellationToken cancellationToken) =>
+            Ok(await _redisCacheService.GetAsync<ShoppingCartDto>(key, cancellationToken));
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCart(ShoppingCartDto shoppingCart, CancellationToken cancellationToken) =>
+            Ok(await _redisCacheService.SetAsync<ShoppingCartDto>(shoppingCart.UserName, shoppingCart, cancellationToken));
+
+        [HttpDelete, Route("key")]
+        public async Task<IActionResult> RemoveCart(string key, CancellationToken cancellationToken) =>
+            Ok(await _redisCacheService.RemoveAsync(key, cancellationToken));
+    }
+}
--- Microservice/Services/Catalog.API/Services/Interfaces/IProductService.cs
+++ Microservice/Services/Catalog.API/Services/Interfaces/IProductService.cs
@@ -5,7 +5,7 @@ namespace Catalog.API.Services.Interfaces
 {
     public interface IProductService : ICommonService<Product> 
     {
-        Task<ProductDto?> GetProductByNameAsync(string name);
-        Task<List<ProductDto>?> GetProductByCategoryNameAsync(string categoryName);
+        Task<ProductDto?> GetProductByNameAsync(string name, CancellationToken cancellationToken = default);
+        Task<List<ProductDto>?> GetProductByCategoryNameAsync(string categoryName, CancellationToken cancellationToken = default);
     }
 }
